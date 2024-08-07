0ae10595fb7d5c8b29a68abf25996b919762cab1
alexandr malitskiy <cfif5665@gmail.com>
2024-08-08 01:14:15 +0600

    ADD: cli support

--- app/main.py
+++ app/main.py
@@ -1,11 +1,18 @@
-import uvicorn
-from src.core.settings import settings
+
+from src.base.user.cli.cli import cli
+from src.repository.postgres_database import PostgresDatabase
+from src.repository.redis_database import RedisDatabase
+from src.factories.repository_factory import RepositoryFactory
+
+RepositoryFactory.register("postgresql", PostgresDatabase)
+RepositoryFactory.register("redis", RedisDatabase)
 
 
 if __name__ == "__main__":
-    uvicorn.run(
-        "app:app",
-        host=settings.host,
-        port=settings.port,
-        reload=True,
-    )
+    # uvicorn.run(
+    #     "app:app",
+    #     host=settings.host,
+    #     port=settings.port,
+    #     reload=True,
+    # )
+    cli()
--- app/src/repository/utils/get_repository.py
+++ app/src/repository/utils/get_repository.py
@@ -1,12 +1,12 @@
+from fastapi import Depends
+
 from src.repository.interfaceses.repository_interface import RepositoryInterface
 from src.factories.repository_factory import RepositoryFactory
 from src.core.settings import settings
-
 
 def get_repository() -> RepositoryInterface:
     repo_type = settings.repository_type
 
     if repo_type in settings.repository_list:
         return RepositoryFactory.create(repo_type)
-    else:
-        raise ValueError("Invalid repository type")
+    raise ValueError("Invalid repository type")
